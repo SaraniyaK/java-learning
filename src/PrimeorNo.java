9fa709c8587294a0957224dad4f5621c372958d7
SaraniyaK <saraniyakumar2006@gmail.com>
2026-04-06 12:15:08 +0530

    day 2 -prime number checker

---
+++ src/PrimeorNo.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class PrimeorNo {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter a Number : ");
+        int num =sc.nextInt();
+        boolean isPrime =true;
+        if(num<=1){
+            isPrime=false;
+        }
+        for (int i=2;i<=num/2;i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if(isPrime){
+            System.out.println(num+" is a Prime Number.");
+        }
+        else{
+            System.out.println(num +" is Not a Prime Number.");
+        }
+    }
+}
