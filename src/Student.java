870490545f5590a26035447444f04f68969cf91e
SaraniyaK <saraniyakumar2006@gmail.com>
2026-04-19 17:34:05 +0530

    Day 5 - Constructor with User input

--- src/Student.java
+++ src/Student.java
@@ -4,26 +4,33 @@ public class Student {
     int age;
     double mark;
 
+    //CONSTRUCTOR
+    Student(String name,int age,double mark){
+        this.name =name;
+        this.age=age;
+        this.mark=mark;
+    }
+
     void display() {
         System.out.println(name + " is " + age + " years old. Student's Mark  is " + mark);
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("How Many Students You want to add? ");
-        int noofstu = sc.nextInt();
-        sc.nextLine();
-        for (int i = 1; i <= noofstu; i++) {
-            Student stu =new Student();//creates a new object for every student (that's why in the loop)
-            System.out.println("Enter Student Name:");
-            stu.name = sc.nextLine();
-            System.out.println("Enter Student Age:");
-            stu.age = sc.nextInt();
-            System.out.println("Enter Student Mark:");
-            stu.mark = sc.nextDouble();
-            sc.nextLine();
-            stu.display();
+       Scanner sc=new Scanner(System.in);
+       System.out.println("How Many Students you Wanna Add? ");
+       int noofstu=sc.nextInt();
+       sc.nextLine();
+       for(int i=1;i<=noofstu;i++){
+           System.out.println("Enter Student Name: ");
+           String stuname=sc.nextLine();
+           System.out.println("Enter Student age: ");
+           int stuage=sc.nextInt();
+           System.out.println("Enter Student Mark: ");
+           double stumark =sc.nextDouble();
+           sc.nextLine();
+           Student stu =new Student(stuname,stuage,stumark);
+           stu.display();
+       }
         }
 
     }
-}
