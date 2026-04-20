3dfe8583877c07e50145ec7b87946a1bdbfcfb3e
SaraniyaK <saraniyakumar2006@gmail.com>
2026-04-20 15:47:10 +0530

    day 6 - encapsulation with getters setters validation

--- src/Student.java
+++ src/Student.java
@@ -1,18 +1,49 @@
 import java.util.*;
 public class Student {
-    String name;
-    int age;
-    double mark;
+    private String name;
+    private int age;
+    private double mark;
 
     //CONSTRUCTOR
     Student(String name,int age,double mark){
         this.name =name;
-        this.age=age;
-        this.mark=mark;
+        setAge(age);
+        setMark(mark);
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        if(age>0) {
+            this.age = age;
+        }
+        else{
+            System.out.println("Invalid Age");
+        }
+    }
+
+    public double getMark(){
+        return mark;
+    }
+    public void setMark(double mark){
+        if(mark>0 && mark<=100) {
+            this.mark = mark;
+        }
+        else{
+            System.out.println("Invalid Mark .Mark should be 0-100");
+        }
     }
 
     void display() {
-        System.out.println(name + " is " + age + " years old. Student's Mark  is " + mark);
+        System.out.println(getName() + " is " + getAge() + " years old. Student's Mark  is " + getMark());
     }
 
     public static void main(String[] args) {
